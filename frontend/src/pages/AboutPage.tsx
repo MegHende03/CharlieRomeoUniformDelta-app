06b6e6e9034ce0a0fb8f9f716cdea4d3ea4e66f6
Meghan <meghende03@yahoo.com>
2026-05-09 16:21:02 -0400

    Created folder structure

---
+++ frontend/src/pages/AboutPage.tsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+    
+    return (
+        <>
+            <p> Hello From About Page!</p> 
+        </>
+    );
+}
+
+export default AboutPage;
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+
+function HomePage() {
+    
+    return (
+        <>
+            <p> Hello From Home Page!</p> 
+        </>
+    );
+}
+
+export default HomePage;
